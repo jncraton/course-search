0c5624b10b7e5c765a1baf63defdee44ee404c4a
Jon Craton <jncraton@gmail.com>
2021-09-10 10:13:56 -0400

    Add main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+import { courses } from './courses.js'
+
+const rows = courses.map((course) => {
+  return `<tr><td>${course.CRSE} - ${course.DESCR}</td></tr>`
+})
+
+document.querySelector('table').innerHTML = rows.join('')
