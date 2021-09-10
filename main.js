0288ac6a16b554a1e966c06bff0c46ff9b595b03
Joshua Huffman <jwhuffman@anderson.edu>
2021-09-10 11:33:17 -0400

    Closed classes highlight in red

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 import { courses } from './courses.js'
-
+let closedClass = "";
 const rows = courses.map((course) => {
-  return `<tr><td>${course.CRSE} - ${course.DESCR}</td></tr>`
+  if (course.ENROLLING === 'Open')
+  {
+    closedClass = "openClass";
+  }
+  else
+  {
+    closedClass = "closedClass";
+  }
+  return `<tr class = ${closedClass}><td>${course.CRSE} - ${course.DESCR}</td></tr>`
 })
+
 
 document.querySelector('tbody').innerHTML = rows.join('')
