302dc5bf4d2325a79b8d7225450955de35ed0002
Justin <jkschy@anderson.edu>
2021-09-13 11:43:35 -0400

    add Docs

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-import { courses } from './courses.js'
+import { courses } from "./courses.js"
 
 let hideRequirement = false
 
-function render () {
-  const rows = courses.map((course) => {
-    /**
+//Renders the table based on the above variables
+function render() {
+	const rows = courses.map((course) => {
+		/**
 		 * Add Filters Here
 		 */
 
-    // Checks if the hide consent checkbox is checked, and if so, returns and does not render
-    if (hideRequirement && course.CONSENT === 'Instructor Consent Required') {
+		// Checks if the hide consent checkbox is checked, and if so, returns and does not render
+		if (hideRequirement && course.CONSENT === "Instructor Consent Required") {
+		} else {
+			return `<tr><td>${course.CRSE}</td><td>${course.DESCR}</td><td>${course.INSTR}</td><td>${course.DAYS}</td></tr>`
+		}
+	})
 
-    } else {
-      return `<tr><td>${course.CRSE}</td><td>${course.DESCR}</td><td>${course.INSTR}</td><td>${course.DAYS}</td></tr>`
-    }
-  })
-
-  document.querySelector('tbody').innerHTML = rows.join('')
+	document.querySelector("tbody").innerHTML = rows.join("")
 }
 
 /*
  * Event listener on checkbox that hides courses requiring special consent
  */
-document.getElementById('hide-courses-requiring-consent').addEventListener('click', () => {
-  hideRequirement = !hideRequirement
-  render()
+document.getElementById("hide-courses-requiring-consent").addEventListener("click", () => {
+	hideRequirement = !hideRequirement
+	render()
 })
 
 render()
