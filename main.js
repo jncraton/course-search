08259b3223910b37aa29c6eaf0bfe5783c5f840a
Justin <jkschy@anderson.edu>
2021-09-13 18:48:39 -0400

    add a button on the class size to sort by that, and a textbox to filter course number

--- main.js
+++ main.js
@@ -1,27 +1,62 @@
 import { courses } from './courses.js'
 
+const classNumberBox = document.getElementById('classNumberFilter')
+const classSizeButton = document.getElementById('filterBySize')
+
 let hideRequirement = false
+let courseNumberFilter = ''
+let classSize = ''
 
 // Renders the table based on the above variables
-function render () {
-  const rows = courses.map((course) => {
-    // Checks if the hide consent checkbox is checked, and if so, returns and does not render add other filters HERE
-    if (hideRequirement && course.CONSENT === 'Instructor Consent Required') {
-      return ''
-    } else {
-      return `<tr><td>${course.CRSE}</td><td>${course.DESCR}</td><td>${course.INSTR}</td><td>${course.DAYS}</td></tr>`
-    }
-  })
+function render() {
+	let sortedCourses = [...courses]
 
-  document.querySelector('tbody').innerHTML = rows.join('')
+	if (classSize) {
+		sortedCourses.sort((a, b) => b.ENROLLED - a.ENROLLED)
+		if (classSize === 'low') sortedCourses.reverse()
+	}
+
+	if (courseNumberFilter) {
+		sortedCourses = sortedCourses.filter((course) => course.CRSE.includes(courseNumberFilter.toUpperCase()))
+	}
+
+	const rows = sortedCourses.map((course) => {
+		// Checks if the hide consent checkbox is checked, and if so, returns and does not render add other filters HERE
+		if (hideRequirement && course.CONSENT === 'Instructor Consent Required') {
+			return ''
+		} else {
+			return `<tr><td>${course.CRSE}</td><td>${course.DESCR}</td><td>${course.INSTR}</td><td>${course.DAYS}</td><td>${course.ENROLLED}</td></tr>`
+		}
+	})
+
+	document.querySelector('tbody').innerHTML = rows.join('')
 }
 
 /*
  * Event listener on checkbox that hides courses requiring special consent
  */
 document.getElementById('hide-courses-requiring-consent').addEventListener('click', () => {
-  hideRequirement = !hideRequirement
-  render()
+	hideRequirement = !hideRequirement
+	render()
+})
+
+classSizeButton.addEventListener('click', () => {
+	if (classSizeButton.innerText.includes('∧')) {
+		classSizeButton.innerText = classSizeButton.innerText.substr(0, classSizeButton.innerText.length - 1) + '∨'
+		classSize = 'low'
+	} else if (classSizeButton.innerText.includes('∨')) {
+		classSizeButton.innerText = classSizeButton.innerText.substr(0, classSizeButton.innerText.length - 1)
+		classSize = ''
+	} else {
+		classSizeButton.innerText += ' ∧'
+		classSize = 'high'
+	}
+	render()
+})
+
+classNumberBox.addEventListener('keyup', () => {
+	courseNumberFilter = classNumberBox.value
+	render()
 })
 
 render()
