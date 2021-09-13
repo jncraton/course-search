a88ce177d714831d439f517243cdb45fe271ca0a
Joshua Huffman <jwhuffman@anderson.edu>
2021-09-13 18:27:10 -0400

    Should make the text of closed courses red

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 import { courses } from './courses.js'
-let closedClass = "";
+let closedClass = ''
 const rows = courses.map((course) => {
-  if (course.ENROLLING === 'Open')
-  {
-    closedClass = "openClass";
-  }
-  else
-  {
-    closedClass = "closedClass";
+  if (course.ENROLLING === 'Open') {
+    closedClass = 'openClass'
+  } else {
+    closedClass = 'closedClass'
   }
   return `<tr class = ${closedClass}><td>${course.CRSE} - ${course.DESCR}</td></tr>`
 })
-
 
 document.querySelector('tbody').innerHTML = rows.join('')
