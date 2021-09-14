a55236295710b10dec35611e4a98da391bfae7c1
Joshua Huffman <jwhuffman@anderson.edu>
2021-09-14 15:29:18 -0400

    trial by fire attempt two

--- main.js
+++ main.js
@@ -1,25 +1,21 @@
 import { courses } from './courses.js'
 
 let closedClass = ''
-const rows = courses.map((course) => {
-  if (course.ENROLLING === 'Open') {
-    closedClass = 'openClass'
-  } else {
-    closedClass = 'closedClass'
-  }
-  return `<tr class = ${closedClass}><td>${course.CRSE} - ${course.DESCR}</td></tr>`
-
-
 let hideRequirement = false
 
 // Renders the table based on the above variables
 function render () {
   const rows = courses.map((course) => {
     // Checks if the hide consent checkbox is checked, and if so, returns and does not render add other filters HERE
+    if (course.ENROLLING === 'Open') {
+      closedClass = 'openClass'
+    } else {
+      closedClass = 'closedClass'
+    }
     if (hideRequirement && course.CONSENT === 'Instructor Consent Required') {
       return ''
     } else {
-      return `<tr><td>${course.CRSE}</td><td>${course.DESCR}</td><td>${course.INSTR}</td><td>${course.DAYS}</td></tr>`
+      return `<tr class = ${closedClass}><td>${course.CRSE}</td><td>${course.DESCR}</td><td>${course.INSTR}</td><td>${course.DAYS}</td></tr>`
     }
   })
 
@@ -32,7 +28,6 @@ function render () {
 document.getElementById('hide-courses-requiring-consent').addEventListener('click', () => {
   hideRequirement = !hideRequirement
   render()
-
 })
 
 render()
