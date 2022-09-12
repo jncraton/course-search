8d6b31bef4df25a64d354e7d2883002caba525b1
emmalee <emmalee.paarlberg@gmail.com>
2022-09-12 09:12:51 -0400

    button + function added for filtering online courses

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 import { courses } from './courses.js'
 
+function showOnlineOnly(){
+  courses.forEach(course => {
+    if(course.INSTRUCTION_MODE != "Online"){
+    }
+  });
+  
+}
+
 const rows = courses.map(course => {
   return `<tr><td>${course.CRSE} - ${course.DESCR}</td></tr>`
 })
