98b918f1953483ac9848a99bf781d8396745cb10
jgalford <jalford@zincpartners.us>
2022-09-12 17:39:07 -0400

    Added the ability to sort by class number or class size.

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 import { courses } from './courses.js'
 
 const rows = courses.map(course => {
-  return `<tr><td>${course.CRSE} - ${course.DESCR}</td></tr>`
+  return `<tr><td>${course.CRSE} - ${course.DESCR} - ${course.ENROLLED}</td></tr>`
 })
 
 document.querySelector('tbody').innerHTML = rows.join('')
+
+const sortByClassNumberEL = document.getElementById('sortByClassNumber')
+sortByClassNumberEL.addEventListener('click', sortByClassNumber)
+function sortByClassNumber() {
+  console.log("sortByClassNumber")
+  courses.sort((a, b) => a.CRSE.localeCompare(b.CRSE))
+  const rows = courses.map(course => {
+    return `<tr><td>${course.CRSE} - ${course.DESCR} - ${course.ENROLLED}</td></tr>`
+  })
+  document.querySelector('tbody').innerHTML = rows.join('')
+}
+const sortByClassSizeEL = document.getElementById('sortByClassSize')
+sortByClassSizeEL.addEventListener('click', sortByClassSize)
+function sortByClassSize() { 
+  console.log("sortByClassSize")
+  courses.sort((a, b) => b.ENROLLED - a.ENROLLED)
+  const rows = courses.map(course => {
+    return `<tr><td>${course.CRSE} - ${course.DESCR} - ${course.ENROLLED}</td></tr>`
+  })
+  document.querySelector('tbody').innerHTML = rows.join('')
+}
