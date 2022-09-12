e6d1051d29451597c5ee803bd9fc702bd6d553bd
fuzorh <uzorhfrancis@gmail.com>
2022-09-12 10:25:56 -0400

    Still in Progress... Trying to pass the UI selection into the javascript filter function.

--- main.js
+++ main.js
@@ -1,7 +1,42 @@
 import { courses } from './courses.js'
 
-const rows = courses.map(course => {
-  return `<tr><td>${course.CRSE} - ${course.DESCR}</td></tr>`
+const depts = new Set(["Choose a department"]);
+courses.map(course => {
+  let dept = course.CRSE.substring(0,4);
+  depts.add(dept);
 })
 
-document.querySelector('tbody').innerHTML = rows.join('')
+const list = Array.from(depts).map(dept => {
+  return `<option value=${dept}>${dept}</option>`
+})
+
+document.querySelector('[name="dept"]').innerHTML = list.join('')
+
+
+function renderCourses() {
+  
+  let filterDept = document.querySelector('[name="dept"]').textContent
+  
+  if (filterDept.length() > 4){
+    const rows = courses.map(course => {
+    return `<tr><td>${course.CRSE} - ${course.DESCR} </td></tr>`
+  })
+
+  document.querySelector('tbody').innerHTML = rows.join('')
+  document.querySelector('[name="dept"]').addEventListener('change',renderCourses)
+
+  }
+  else {
+    filteredCourses = courses.filter(course => {
+    return course.CRSE.substring(0,4) === filterDept
+  })
+  const rows = filteredCourses.map(course => {
+    return `<tr><td>${course.CRSE} - ${course.DESCR} </td></tr>`
+  })
+
+  document.querySelector('tbody').innerHTML = rows.join('')
+  document.querySelector('[name="dept"]').addEventListener('change',renderCourses)
+
+  }
+}
+renderCourses();
