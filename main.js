43f5b63466e009eec2028cbb5b0492e5cf17d204
Michael Carroll <mrcarroll@anderson.edu>
2022-09-12 14:22:51 -0400

    adds support to search with any casing

--- main.js
+++ main.js
@@ -5,12 +5,21 @@ import { courses } from './courses.js'
 document.getElementById('string-search').addEventListener('click', getCourses)
 //Get courses lets the user search for a course by CRSE, DESCR, or INSTR
 function getCourses() {
+  let num; // to store course number
+  let desc; // to store course description
+  let inst; // to store course instructor
   //get value from box
   let input = document.getElementById('course-search-box').value
+  //convert to uppercase
+  input = input.toUpperCase()
   //loop through courses
   const rows = courses.map(course => {
+    // convert to uppercase
+    num = course.CRSE.toUpperCase();
+    desc = course.DESCR.toUpperCase();
+    inst = course.INSTR.toUpperCase();
     //if input is in CRSE, DESCR, or INSTR, return course
-    if (course.CRSE.includes(input) || course.DESCR.includes(input) || course.INSTR.includes(input)) {
+    if (num.includes(input) || desc.includes(input) || inst.includes(input)) {
       //build table
       return `<tr><td>${course.CRSE} - ${course.DESCR} - ${course.INSTR}</td></tr>`
     }
