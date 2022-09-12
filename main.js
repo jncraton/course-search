381a4dcc232f5d72be7a83e596bbc9666473f2ac
Michael Carroll <mrcarroll@anderson.edu>
2022-09-12 10:41:40 -0400

    adds search by desc, num, or inst

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 import { courses } from './courses.js'
 
+//Author: Michael Carroll
+//event listener for search button
+document.getElementById('string-search').addEventListener('click', getCourses)
+//Get courses lets the user search for a course by CRSE, DESCR, or INSTR
+function getCourses() {
+  //get value from box
+  let input = document.getElementById('course-search-box').value
+  //loop through courses
+  const rows = courses.map(course => {
+    //if input is in CRSE, DESCR, or INSTR, return course
+    if (course.CRSE.includes(input) || course.DESCR.includes(input) || course.INSTR.includes(input)) {
+      //build table
+      return `<tr><td>${course.CRSE} - ${course.DESCR} - ${course.INSTR}</td></tr>`
+    }
+  })
+  //join rows
+  document.querySelector('tbody').innerHTML = rows.join('')
+  //clear input box
+  document.getElementById('course-search-box').value = ''
+}
+
 const rows = courses.map(course => {
   return `<tr><td>${course.CRSE} - ${course.DESCR}</td></tr>`
 })
