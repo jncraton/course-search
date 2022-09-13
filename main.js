00ebd76d037002b31c0d8d686e2b1274056f8b69
emmalee <emmalee.paarlberg@gmail.com>
2022-09-13 10:34:17 -0400

    prettier lint fix

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 import { courses } from './courses.js'
 
-let filteredCourses = courses;
-let online = false;
+let filteredCourses = courses
+let online = false
 
 // Add event listener to online only button
-const onlineButtonEL = document.getElementById("online-filter");
-onlineButtonEL.addEventListener("click", showOnlineOnly, false);
+const onlineButtonEL = document.getElementById('online-filter')
+onlineButtonEL.addEventListener('click', showOnlineOnly, false)
 
 //filters the course list by whether or not the course is online
 //activated when the show online courses only button is clicked
-function showOnlineOnly(){
-  if(online){
-    filteredCourses = courses;
-    online = false;
-  }
-  else{
-     filteredCourses = courses.filter(function(value,index){
-      return value.INSTRUCTION_MODE == "Online";
-  })
-  online = true;
+function showOnlineOnly() {
+  if (online) {
+    filteredCourses = courses
+    online = false
+  } else {
+    filteredCourses = courses.filter(function (value, index) {
+      return value.INSTRUCTION_MODE == 'Online'
+    })
+    online = true
   }
   let rows = filteredCourses.map(course => {
     return `<tr><td>${course.CRSE} - ${course.DESCR} - ${course.INSTRUCTION_MODE}</td></tr>`
