a3ac178563802bf8c63323c6068cc718bb7c3077
Mackerell <64169117+Mackerell-Baka@users.noreply.github.com>
2022-09-13 15:24:52 -0400

    Add Closed Collumn

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { courses } from './courses.js'
 
 const rows = courses.map(course => {
-  return `<tr><td>${course.CRSE} - ${course.DESCR}</td></tr>`
+  return `<tr><td>${course.CRSE} - ${course.DESCR} - ${course.ENROLLING} - </td></tr>`
 })
 
 document.querySelector('tbody').innerHTML = rows.join('')
