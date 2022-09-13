a1577e90183241e184812f9a2c9512a2d5d04f0b
jgalford <jalford@zincpartners.us>
2022-09-13 10:40:31 -0400

    Fixed Prettier

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ document.querySelector('tbody').innerHTML = rows.join('')
 const sortByClassNumberEL = document.getElementById('sortByClassNumber')
 sortByClassNumberEL.addEventListener('click', sortByClassNumber)
 function sortByClassNumber() {
-  console.log("sortByClassNumber")
+  console.log('sortByClassNumber')
   courses.sort((a, b) => a.CRSE.localeCompare(b.CRSE))
   const rows = courses.map(course => {
     return `<tr><td>${course.CRSE} - ${course.DESCR} - ${course.ENROLLED}</td></tr>`
@@ -21,8 +21,8 @@ function sortByClassNumber() {
 //sorts the table by class size when the button is clicked
 const sortByClassSizeEL = document.getElementById('sortByClassSize')
 sortByClassSizeEL.addEventListener('click', sortByClassSize)
-function sortByClassSize() { 
-  console.log("sortByClassSize")
+function sortByClassSize() {
+  console.log('sortByClassSize')
   courses.sort((a, b) => b.ENROLLED - a.ENROLLED)
   const rows = courses.map(course => {
     return `<tr><td>${course.CRSE} - ${course.DESCR} - ${course.ENROLLED}</td></tr>`
