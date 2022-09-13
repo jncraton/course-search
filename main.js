b8f4cf1b1087804e4f33a5e31ee1bf87cf7b2db2
jdspires00 <jdspires@anderson.edu>
2022-09-13 10:19:53 -0400

    Created search box for classes based on start time

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 import { courses } from './courses.js'
 
+//Author: @Jacob Spires
+document.getElementById('search-button').addEventListener('click', getCoursesFromTime)
+//getCoursesFromTime lets the user search for a course based on start time
+function getCoursesFromTime() {
+  let input = document.getElementById('timeSearch').value
+  input = input.toUpperCase() //accounts for lower case input
+  const rows = courses.map(course => {
+    //if input is in START_TIME, return the start time, course name, and course description
+    if (course.START_TIME.includes(input)) {
+      return `<tr><td>${course.START_TIME} - ${course.CRSE} - ${course.DESCR}</td></tr>`
+    }
+  })
+  document.querySelector('tbody').innerHTML = rows.join('')
+}
 const rows = courses.map(course => {
-  return `<tr><td>${course.CRSE} - ${course.DESCR}</td></tr>`
+  return `<tr><td>${course.START_TIME} - ${course.CRSE} - ${course.DESCR}</td></tr>`
 })
-
 document.querySelector('tbody').innerHTML = rows.join('')
