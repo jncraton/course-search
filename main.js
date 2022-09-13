c77b3fcd7ea7d8dee06d3dc3d8fd52fbabdfff57
Jon Craton <jncraton@gmail.com>
2022-09-13 10:36:18 -0400

    Run prettier

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import { courses } from './courses.js'
 
 //Populate Dropdown
-const depts = new Set();
+const depts = new Set()
 courses.map(course => {
-  let dept = course.CRSE.substring(0,4);
-  depts.add(dept);
+  let dept = course.CRSE.substring(0, 4)
+  depts.add(dept)
 })
 
 const list = Array.from(depts).map(dept => {
@@ -17,14 +17,12 @@ document.querySelector('[name="dept"]').innerHTML = list.join('')
 
 //Populate Courses
 function renderCourses() {
-  
   let filterDept = document.querySelector('[name="dept"]').value
 
-  
   let filteredCourses = courses
-  if(filterDept) {
-      filteredCourses = courses.filter(course => {
-      return course.CRSE.substring(0,4) === filterDept
+  if (filterDept) {
+    filteredCourses = courses.filter(course => {
+      return course.CRSE.substring(0, 4) === filterDept
     })
   }
   const rows = filteredCourses.map(course => {
@@ -34,5 +32,7 @@ function renderCourses() {
   document.querySelector('tbody').innerHTML = rows.join('')
 }
 
-document.querySelector('[name="dept"]').addEventListener('change',renderCourses)
-renderCourses();
+document
+  .querySelector('[name="dept"]')
+  .addEventListener('change', renderCourses)
+renderCourses()
