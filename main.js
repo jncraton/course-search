fda2ed67918c9faa393ba8dbe4116b43997855d9
Michael Carroll <mrcarroll@anderson.edu>
2022-09-14 08:58:25 -0400

    'ran prettier fix'

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ import { courses } from './courses.js'
 document.getElementById('string-search').addEventListener('click', getCourses)
 //Get courses lets the user search for a course by CRSE, DESCR, or INSTR
 function getCourses() {
-  let num; // to store course number
-  let desc; // to store course description
-  let inst; // to store course instructor
+  let num // to store course number
+  let desc // to store course description
+  let inst // to store course instructor
   //get value from box
   let input = document.getElementById('course-search-box').value
   //convert to uppercase
@@ -15,9 +15,9 @@ function getCourses() {
   //loop through courses
   const rows = courses.map(course => {
     // convert to uppercase
-    num = course.CRSE.toUpperCase();
-    desc = course.DESCR.toUpperCase();
-    inst = course.INSTR.toUpperCase();
+    num = course.CRSE.toUpperCase()
+    desc = course.DESCR.toUpperCase()
+    inst = course.INSTR.toUpperCase()
     //if input is in CRSE, DESCR, or INSTR, return course
     if (num.includes(input) || desc.includes(input) || inst.includes(input)) {
       //build table
