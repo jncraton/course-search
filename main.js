38bf89572e4a7d4705a9e5ec43e8180ed6b480fd
Michael Carroll <mrcarroll@anderson.edu>
2022-09-17 14:08:00 -0400

    fixes prettier lint

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ import { courses } from './courses.js'
 
 //Accepts an array and returns every course with input included in DESCR
 function searchDescr(courses, searchTerm) {
-  return courses.filter(course => course.DESCR.toLowerCase().includes(searchTerm.toLowerCase()))
+  return courses.filter(course =>
+    course.DESCR.toLowerCase().includes(searchTerm.toLowerCase())
+  )
 }
 
 //Accepts an array and returns every course with input included in INSTR
 function searchInstr(courses, searchTerm) {
-  return courses.filter(course => course.INSTR.toLowerCase().includes(searchTerm.toLowerCase()))
+  return courses.filter(course =>
+    course.INSTR.toLowerCase().includes(searchTerm.toLowerCase())
+  )
 }
 
 const rows = courses.map(course => {
