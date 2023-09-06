38f2e6cc17bc81061dc83a2664534d9689f52860
Jon Craton <jncraton@gmail.com>
2023-09-06 12:03:44 -0400

    Adjust line breaks

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import { courses } from './courses.js'
 
 const rows = courses.map(course => {
-  return `<tr><td>${course.CRSE} - ${course.DESCR}</td></tr>`
+  return `<tr>
+            <td>${course.CRSE} - ${course.DESCR}</td>
+          </tr>`
 })
 
 document.querySelector('tbody').innerHTML = rows.join('')
