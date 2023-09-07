a8ab1fc65607f9facc82f7dd2805535b7e948789
Caburke1218 <143521932+Caburke1218@users.noreply.github.com>
2023-09-07 13:18:04 -0400

    Sorting Issue (#78)
    * added dropdown for filter by course number and enrolled students
    
    * ran linter

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
 import { courses } from './courses.js'
 
-const rows = courses.map(course => {
-  if (course.ENROLLING == 'Closed') {
-    return `<tr>
-    <td><s>${course.CRSE} - ${course.DESCR}</s></td>
-  </tr>`
-  } else {
-    return `<tr>
-    <td>${course.CRSE} - ${course.DESCR}</td>
-  </tr>`
-  }
-})
+const courseTableBody = document.querySelector('tbody')
+const sortSelect = document.getElementById('sortSelect')
 
-document.querySelector('tbody').innerHTML = rows.join('')
+function renderCourses(courses, sortBy) {
+  const rows = courses.map(course => {
+    if (course.ENROLLING == 'Closed') {
+      return `<tr>
+      <td><s>${course.CRSE} - ${course.DESCR}</s></td>
+    </tr>`
+    } else {
+      return `<tr>
+      <td>${course.CRSE} - ${course.DESCR}</td>
+    </tr>`
+    }
+  })
+
+  courseTableBody.innerHTML = rows.join('')
+}
+
+// Initial render
+renderCourses(courses, 'courseNumber')
+
+// Event listener for sorting
+sortSelect.addEventListener('change', () => {
+  const sortBy = sortSelect.value
+
+  if (sortBy === 'courseNumber') {
+    courses.sort((a, b) => a.CRSE.localeCompare(b.CRSE))
+  } else if (sortBy === 'enrolled') {
+    courses.sort((a, b) => parseInt(b.ENROLLED) - parseInt(a.ENROLLED))
+  } else {
+    // Default sorting or invalid option
+    courses.sort((a, b) => a.DESCR.localeCompare(b.DESCR))
+  }
+
+  renderCourses(courses, sortBy)
+})
