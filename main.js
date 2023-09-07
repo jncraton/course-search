323da9862225c8f40a774e8e46b2f30f74a2fdad
Marcus Shaffer <101298098+shama2025@users.noreply.github.com>
2023-09-07 10:02:24 -0400

    changed main.js (#77)
    * changed main.js
    
    Added strike through to closed courses to show the user which courses are closed.
    
    * Run Prettier
    
    ---------
    
    Co-authored-by: scoutskylar <scoutskylar@outlook.com>

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 import { courses } from './courses.js'
 
 const rows = courses.map(course => {
-  return `<tr>
-            <td>${course.CRSE} - ${course.DESCR}</td>
-          </tr>`
+  if (course.ENROLLING == 'Closed') {
+    return `<tr>
+    <td><s>${course.CRSE} - ${course.DESCR}</s></td>
+  </tr>`
+  } else {
+    return `<tr>
+    <td>${course.CRSE} - ${course.DESCR}</td>
+  </tr>`
+  }
 })
 
 document.querySelector('tbody').innerHTML = rows.join('')
