af508e3848845d814b6954e25cdd90d5e1fd9080
KedricNees01 <kedricnees@gmail.com>
2023-09-07 14:04:29 -0400

    Add online class filter

--- main.js
+++ main.js
@@ -7,3 +7,43 @@ const rows = courses.map(course => {
 })
 
 document.querySelector('tbody').innerHTML = rows.join('')
+
+function filterAll(){
+  const rows = courses.map(course => {
+    return `<tr>
+              <td>${course.CRSE} - ${course.DESCR}</td>
+            </tr>`
+  })
+  document.querySelector('tbody').innerHTML = rows.join('')
+}
+
+function filterOnline() {
+  const onlineCourses = courses.filter((course) => course.INSTRUCTION_MODE == "Online");
+  const rows2 = onlineCourses.map((course) => {
+    return `<tr>
+              <td>${course.CRSE} - ${course.DESCR}</td>
+            </tr>`;
+  });
+  document.querySelector('tbody').innerHTML = rows2.join('');
+}
+
+function filterFace(){
+  const onlineCourses = courses.filter((course) => course.INSTRUCTION_MODE == "Face to Face");
+  const rows = onlineCourses.map((course) => {
+    return `<tr>
+              <td>${course.CRSE} - ${course.DESCR}</td>
+            </tr>`;
+  });
+
+  document.querySelector('tbody').innerHTML = rows.join('');
+}
+
+function filterHybrid(){
+  const onlineCourses = courses.filter((course) => course.INSTRUCTION_MODE == "Blended:Mtg/Online");
+  const rows = onlineCourses.map((course) => {
+    return `<tr>
+              <td>${course.CRSE} - ${course.DESCR}</td>
+            </tr>`;
+  });
+  document.querySelector('tbody').innerHTML = rows.join('');
+}
