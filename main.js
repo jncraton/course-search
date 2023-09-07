703fe61ceffe4ec90e95acfc588e7c64a9b219ff
Caleb Burke <caburke2@anderson.edu>
2023-09-07 10:52:20 -0400

    ran linter

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-import { courses } from './courses.js';
+import { courses } from './courses.js'
 
-const courseTableBody = document.querySelector('tbody');
-const sortSelect = document.getElementById('sortSelect');
+const courseTableBody = document.querySelector('tbody')
+const sortSelect = document.getElementById('sortSelect')
 
 function renderCourses(courses, sortBy) {
-  const rows = courses.map((course) => {
+  const rows = courses.map(course => {
     if (course.ENROLLING == 'Closed') {
       return `<tr>
       <td><s>${course.CRSE} - ${course.DESCR}</s></td>
@@ -14,26 +14,26 @@ function renderCourses(courses, sortBy) {
       <td>${course.CRSE} - ${course.DESCR}</td>
     </tr>`
     }
-  });
+  })
 
-  courseTableBody.innerHTML = rows.join('');
+  courseTableBody.innerHTML = rows.join('')
 }
 
 // Initial render
-renderCourses(courses, 'courseNumber');
+renderCourses(courses, 'courseNumber')
 
 // Event listener for sorting
 sortSelect.addEventListener('change', () => {
-  const sortBy = sortSelect.value;
+  const sortBy = sortSelect.value
 
   if (sortBy === 'courseNumber') {
-    courses.sort((a, b) => a.CRSE.localeCompare(b.CRSE));
+    courses.sort((a, b) => a.CRSE.localeCompare(b.CRSE))
   } else if (sortBy === 'enrolled') {
-    courses.sort((a, b) => parseInt(b.ENROLLED) - parseInt(a.ENROLLED));
+    courses.sort((a, b) => parseInt(b.ENROLLED) - parseInt(a.ENROLLED))
   } else {
     // Default sorting or invalid option
-    courses.sort((a, b) => a.DESCR.localeCompare(b.DESCR));
+    courses.sort((a, b) => a.DESCR.localeCompare(b.DESCR))
   }
 
-  renderCourses(courses, sortBy);
-});
+  renderCourses(courses, sortBy)
+})
