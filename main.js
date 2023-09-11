85f4f3768f068bf0049292f8189cc28980415435
Mason Wallace <mdwallace2@anderson.edu>
2023-09-11 16:04:40 -0400

    Fix merge conflicts

--- main.js
+++ main.js
@@ -1,53 +1,53 @@
-import { courses } from './courses.js';
+import { courses } from './courses.js'
 
 // Create set that stores departments first 4 characters and doesnt repeat
-const departmentSet = new Set();
+const departmentSet = new Set()
 
 courses.forEach(course => {
-  const department = course.CRSE.slice(0, 4);
-  departmentSet.add(department);
-});
+  const department = course.CRSE.slice(0, 4)
+  departmentSet.add(department)
+})
 
 // Populate dropdown menu
-const departmentsDropdown = document.getElementById('departments');
+const departmentsDropdown = document.getElementById('departments')
 departmentSet.forEach(department => {
-  const option = document.createElement('option');
-  option.value = department;
-  option.textContent = `Department ${department}`;
-  departmentsDropdown.appendChild(option);
-});
+  const option = document.createElement('option')
+  option.value = department
+  option.textContent = `Department ${department}`
+  departmentsDropdown.appendChild(option)
+})
 
 const rows = courses.map(course => {
   return `<tr>
             <td>${course.CRSE} - ${course.DESCR}</td>
-          </tr>`;
-});
+          </tr>`
+})
 
 //Function updates table based on the department that was selected
 function updateTable(selectedDepartment) {
   const filteredCourses = selectedDepartment
     ? courses.filter(course => {
-        const department = course.CRSE.slice(0, 4);
-        return department == selectedDepartment;
+        const department = course.CRSE.slice(0, 4)
+        return department == selectedDepartment
       })
-    : courses;
+    : courses
 
   const rows = filteredCourses.map(course => {
     return `<tr>
               <td>${course.CRSE} - ${course.DESCR}</td>
-            </tr>`;
-  });
+            </tr>`
+  })
 
-  document.querySelector('tbody').innerHTML = rows.join('');
+  document.querySelector('tbody').innerHTML = rows.join('')
 }
 
 departmentsDropdown.addEventListener('change', event => {
-  const selectedDepartment = event.target.value;
-  updateTable(selectedDepartment);
-});
+  const selectedDepartment = event.target.value
+  updateTable(selectedDepartment)
+})
 
-updateTable('');
-document.querySelector('tbody').innerHTML = rows.join('');
+updateTable('')
+document.querySelector('tbody').innerHTML = rows.join('')
 const courseTableBody = document.querySelector('tbody')
 const sortSelect = document.getElementById('sortSelect')
 
