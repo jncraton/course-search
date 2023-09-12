f3b639e2b2ace942674169252bb5d6a7893108dd
KedricNees01 <97705596+KedricNees01@users.noreply.github.com>
2023-09-12 15:03:11 -0400

    Add online class filter (#79)
    * Add online class filter
    
    * Fix the broken buttons
    
    * Fix code merge issue
    
    * Fix problem with prettier

--- main.js
+++ main.js
@@ -85,3 +85,49 @@ sortSelect.addEventListener('change', () => {
 
   renderCourses(courses, sortBy)
 })
+
+all.onclick = function filterAll() {
+  const rows = courses.map(course => {
+    return `<tr>
+              <td>${course.CRSE} - ${course.DESCR}</td>
+            </tr>`
+  })
+  document.querySelector('tbody').innerHTML = rows.join('')
+}
+
+online.onclick = function filterOnline() {
+  const onlineCourses = courses.filter(
+    course => course.INSTRUCTION_MODE == 'Online',
+  )
+  const rows2 = onlineCourses.map(course => {
+    return `<tr>
+              <td>${course.CRSE} - ${course.DESCR}</td>
+            </tr>`
+  })
+  document.querySelector('tbody').innerHTML = rows2.join('')
+}
+
+face.onclick = function filterFace() {
+  const onlineCourses = courses.filter(
+    course => course.INSTRUCTION_MODE == 'Face to Face',
+  )
+  const rows = onlineCourses.map(course => {
+    return `<tr>
+              <td>${course.CRSE} - ${course.DESCR}</td>
+            </tr>`
+  })
+
+  document.querySelector('tbody').innerHTML = rows.join('')
+}
+
+hybrid.onclick = function filterHybrid() {
+  const onlineCourses = courses.filter(
+    course => course.INSTRUCTION_MODE == 'Blended:Mtg/Online',
+  )
+  const rows = onlineCourses.map(course => {
+    return `<tr>
+              <td>${course.CRSE} - ${course.DESCR}</td>
+            </tr>`
+  })
+  document.querySelector('tbody').innerHTML = rows.join('')
+}
