991a01273372c92dcbf827db797b49740cab2ff5
JWestrumAnderson <91626402+JWestrumAnderson@users.noreply.github.com>
2023-09-19 15:12:23 -0400

    Fix Dropdown filter (#93)
    * Fix filter table by dropdown
    
    * Fix sortBy enrolled students
    
    * Fix time dropdown order
    
    * Run lint
    
    * Run lint again
    
    ---------
    
    Co-authored-by: JWestrum_Zinc <jwestrum@zincpartners.us>

--- main.js
+++ main.js
@@ -11,6 +11,12 @@ const columns = [
   'ENROLLED',
 ]
 
+// Grab element for sorter
+const selectSort = document.getElementById('selectSort')
+selectSort.addEventListener('change', () => {
+  updateTable()
+})
+
 // Create a Set that stores the subject code, which is the first four characters of the course caption
 const subjectSet = new Set()
 courses.forEach(course => {
@@ -26,24 +32,15 @@ Array.from(subjectSet)
     option.textContent = value
     selectSubjectElement.appendChild(option)
   })
-selectSubjectElement.addEventListener('change', updateTable)
-
-// Create a Set that stores the start times
-const startTimeSet = new Set()
-courses.forEach(course => {
-  startTimeSet.add(course.START_TIME)
+selectSubjectElement.addEventListener('change', () => {
+  updateTable()
 })
+
 // Populate start time filter
 const selectStartTimeElement = document.getElementById('selectStartTime')
-Array.from(startTimeSet)
-  .sort()
-  .forEach(value => {
-    const option = document.createElement('option')
-    option.value = value
-    option.textContent = value
-    selectStartTimeElement.appendChild(option)
-  })
-selectStartTimeElement.addEventListener('change', updateTable)
+selectStartTimeElement.addEventListener('change', () => {
+  updateTable()
+})
 
 // Create a Set that stores the instruction modes
 const instructionModeSet = new Set()
@@ -62,7 +59,9 @@ Array.from(instructionModeSet)
     option.textContent = value
     selectInstructionModeElement.appendChild(option)
   })
-selectInstructionModeElement.addEventListener('change', updateTable)
+selectInstructionModeElement.addEventListener('change', () => {
+  updateTable()
+})
 
 // Create a Set that stores the special consent options
 const specialConsentSet = new Set()
@@ -79,21 +78,25 @@ Array.from(specialConsentSet)
     option.textContent = value
     specialConsentElement.appendChild(option)
   })
-specialConsentElement.addEventListener('change', updateTable)
+specialConsentElement.addEventListener('change', () => {
+  updateTable()
+})
 
-// Event listener for sorting dropdown
-selectSort.addEventListener('change', updateTable)
-
+// Element table body
 const coursesTableBody = document.getElementById('coursesTableBody')
 
-// Update table rows given the search parameter
 function updateTable() {
   // Clear all existing rows
   coursesTableBody.innerHTML = ''
+  const setSubject = selectSubjectElement.value
+  const setTime = selectStartTimeElement.value
+  const setInstrction = selectInstructionModeElement.value
+  const setConsent = specialConsentElement.value
+  const setSort = selectSort.value
 
   // Filter courses based on the search query and other filters
   const searchQuery = searchInput.value
-  const filteredCourses = courses.filter(course => {
+  const searchCourses = courses.filter(course => {
     // Check if the course name (DESCR) contains the search query
     return (
       course.CRSE.toLowerCase().includes(searchQuery.toLowerCase()) ||
@@ -102,26 +105,138 @@ function updateTable() {
     )
   })
 
-  // Add a row to the table for each filtered course
+  // Filter Courses
+  const filteredCourses = searchCourses.filter(course => {
+    // No filter
+    if (
+      setSubject === 'All' &&
+      setTime === 'All' &&
+      setInstrction === 'All' &&
+      setConsent === 'All'
+    ) {
+      return courses
+    }
+    // Single filter
+    else if (
+      setTime === 'All' &&
+      setInstrction === 'All' &&
+      setConsent === 'All'
+    ) {
+      return course.CRSE.slice(0, 4) == setSubject
+    } else if (
+      setSubject === 'All' &&
+      setInstrction === 'All' &&
+      setConsent === 'All'
+    ) {
+      return course.START_TIME == setTime
+    } else if (
+      setSubject === 'All' &&
+      setTime === 'All' &&
+      setConsent === 'All'
+    ) {
+      return course.INSTRUCTION_MODE == setInstrction
+    } else if (
+      setSubject === 'All' &&
+      setTime === 'All' &&
+      setInstrction === 'All'
+    ) {
+      return course.CONSENT == setConsent
+    }
+    // 2 filters
+    else if (setSubject === 'All' && setTime === 'All') {
+      return (
+        course.CONSENT == setConsent && course.INSTRUCTION_MODE == setInstrction
+      )
+    } else if (setSubject === 'All' && setInstrction === 'All') {
+      return course.CONSENT == setConsent && course.START_TIME == setTime
+    } else if (setSubject === 'All' && setConsent === 'All') {
+      return (
+        course.INSTRUCTION_MODE == setInstrction && course.START_TIME == setTime
+      )
+    } else if (setTime === 'All' && setInstrction === 'All') {
+      return (
+        course.CRSE.slice(0, 4) == setSubject && course.CONSENT == setConsent
+      )
+    } else if (setTime === 'All' && setConsent === 'All') {
+      return (
+        course.CRSE.slice(0, 4) == setSubject &&
+        course.INSTRUCTION_MODE == setInstrction
+      )
+    } else if (setInstrction === 'All' && setConsent === 'All') {
+      return (
+        course.CRSE.slice(0, 4) == setSubject && course.START_TIME == setTime
+      )
+    }
+    // 3 filters
+    else if (setSubject === 'All') {
+      return (
+        course.START_TIME == setTime &&
+        course.INSTRUCTION_MODE == setInstrction &&
+        course.CONSENT == setConsent
+      )
+    } else if (setTime === 'All') {
+      return (
+        course.CRSE.slice(0, 4) == setSubject &&
+        course.INSTRUCTION_MODE == setInstrction &&
+        course.CONSENT == setConsent
+      )
+    } else if (setInstrction === 'All') {
+      return (
+        course.CRSE.slice(0, 4) == setSubject &&
+        course.START_TIME == setTime &&
+        course.CONSENT == setConsent
+      )
+    } else if (setConsent === 'All') {
+      return (
+        course.CRSE.slice(0, 4) == setSubject &&
+        course.INSTRUCTION_MODE == setInstrction &&
+        course.START_TIME == setTime
+      )
+    }
+    // All filters
+    else {
+      return (
+        course.CRSE.slice(0, 4) == setSubject &&
+        course.START_TIME == setTime &&
+        course.INSTRUCTION_MODE == setInstrction &&
+        course.CONSENT == setConsent
+      )
+    }
+  })
+
+  // No filter (load in filter)
+  if (!filteredCourses) {
+    filteredCourses = courses
+  }
+
+  // Sort by setSort value
+  if (setSort === 'All') {
+    filteredCourses.sort((a, b) => a.CRSE.localeCompare(b.CRSE))
+  } else if (setSort === 'enrolled') {
+    filteredCourses.sort((a, b) => parseInt(b.ENROLLED) - parseInt(a.ENROLLED))
+  }
+
+  // Add a row to the table for each course
   filteredCourses.forEach(course => {
     const row = document.createElement('tr')
+
     if (course.ENROLLING === 'Closed') row.classList.add('closed-course')
     columns.forEach(columnID => {
       const cell = document.createElement('td')
-      // Handle the CONSENT column separately as in your existing code
+
+      // Updates 'Consent' column to shorted field
       if (columnID === 'CONSENT') {
-        if (course.CONSENT === 'No Special Consent Required')
+        if (course.CONSENT == 'No Special Consent Required')
           cell.innerText = 'None'
         else if (course.CONSENT === 'Instructor Consent Required')
           cell.innerText = 'Instructor'
         else if (course.CONSENT === 'Department Consent Required')
           cell.innerText = 'Department'
         else cell.innerText = course[columnID]
-      } else {
-        cell.innerText = course[columnID]
-      }
+      } else cell.innerText = course[columnID]
       row.appendChild(cell)
     })
+
     coursesTableBody.appendChild(row)
   })
 }
@@ -131,4 +246,4 @@ searchInput.setAttribute('size', searchInput.getAttribute('placeholder').length)
 searchInput.addEventListener('input', updateTable)
 
 // Initial unfiltered table
-updateTable()
+updateTable('')
