d5fc1f31e533e55ffcab5c3d6d478662c14da956
Dylan <dtlemon@anderson.edu>
2024-09-06 10:06:04 -0400

    Add Special Consent filter

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 import { courses } from './courses.js'
 
+// However the filter is being applied, we'll assume "hide_special_consent = false; and is true when applied"
+const hide_special_consent = true;
+
 const rows = courses.map(course => {
-  return `<tr>
+  if (hide_special_consent == true) {
+  	if (course.CONSENT != 'No Special Consent Required') {
+  	  return `<tr>
+  	  		<td>Not allowed</td>
+  	  		</tr>`
+  		}
+  	else {
+      return `<tr>
             <td>${course.CRSE} - ${course.DESCR}</td>
           </tr>`
+          }
+    }
 })
 
 document.querySelector('tbody').innerHTML = rows.join('')
