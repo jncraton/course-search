572bbadada29bbddc09bdde43666daf5561b1b19
JIckes12 <99823782+JIckes12@users.noreply.github.com>
2024-09-06 10:07:15 -0400

    Add color for closed classes

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 import { courses } from './courses.js'
 
 const rows = courses.map(course => {
+  //make closed courses highlighted a different color
+  if (course.STATUS == 'Closed') {
+    document. body. style. backgroundColor = 'red';
+  }
   return `<tr>
             <td>${course.CRSE} - ${course.DESCR}</td>
           </tr>`
 })
+
+
 
 document.querySelector('tbody').innerHTML = rows.join('')
