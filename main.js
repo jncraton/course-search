cc79fa9fe642c754c99e13477feee977a25833e2
Dylan <dtlemon@anderson.edu>
2024-09-11 10:39:05 -0400

    Add pre-req filter

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 import { courses } from './courses.js'
 
-// However the filter is being applied, we'll assume "hide_special_consent = false; and is true when applied"
-const hide_special_consent = true;
-
 const rows = courses.map(course => {
-  if (hide_special_consent == true) {
-  	if (course.CONSENT != 'No Special Consent Required') {
-  	  return}
-  	else {
+  if (document.getElementById('pre-req' == true)) {
+    if (course.CONSENT != 'No Special Consent Required') {
+      return
+    } else {
       return `<tr>
             <td>${course.CRSE} - ${course.DESCR}</td>
           </tr>`
-          }
     }
+  } else {
+    return `<tr>
+          <td>${course.CRSE} - ${course.DESCR}</td>
+        </tr>`
+  }
 })
 
 document.querySelector('tbody').innerHTML = rows.join('')
