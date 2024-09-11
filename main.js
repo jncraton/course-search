e165177d145258ec14e45724647c1426b7c2badb
AndrewG <amgeiger@anderson.edu>
2024-09-11 10:14:39 -0400

    Add a button to sort courses by credit hours

--- main.js
+++ main.js
@@ -2,8 +2,20 @@ import { courses } from './courses.js'
 
 const rows = courses.map(course => {
   return `<tr>
-            <td>${course.CRSE} - ${course.DESCR}</td>
+            <td>${course.CRSE} - ${course.DESCR} - ${course.MAX_CREDIT}</td>
           </tr>`
 })
 
 document.querySelector('tbody').innerHTML = rows.join('')
+
+const credSort = document.getElementById("CredSort")
+credSort.onclick = () => {
+  courses.sort((a, b) => b.MAX_CREDIT - a.MAX_CREDIT)
+  const sortedRows = courses.map(course => {
+    return `<tr>
+              <td>${course.CRSE} - ${course.DESCR} - ${course.MAX_CREDIT}</td>
+            </tr>`
+  })
+
+  document.querySelector('tbody').innerHTML = sortedRows.join('')
+};
