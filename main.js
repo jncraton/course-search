47813eefa5f6552fccdd079d060cc678a5bfc884
AndrewG <amgeiger@anderson.edu>
2024-09-11 10:53:55 -0400

    Move button functionality to tool bar and add rows to table

--- main.js
+++ main.js
@@ -2,20 +2,25 @@ import { courses } from './courses.js'
 
 const rows = courses.map(course => {
   return `<tr>
-            <td>${course.CRSE} - ${course.DESCR} - ${course.MAX_CREDIT}</td>
+            <td>${course.CRSE} - ${course.DESCR}</td>
+            <td>${course.MAX_CREDIT}</td>
+            <td>${course.ENROLLED}</td>
+
           </tr>`
 })
 
 document.querySelector('tbody').innerHTML = rows.join('')
 
-const credSort = document.getElementById("CredSort")
+const credSort = document.getElementById('credits')
 credSort.onclick = () => {
   courses.sort((a, b) => b.MAX_CREDIT - a.MAX_CREDIT)
   const sortedRows = courses.map(course => {
     return `<tr>
-              <td>${course.CRSE} - ${course.DESCR} - ${course.MAX_CREDIT}</td>
+              <td>${course.CRSE} - ${course.DESCR}</td>
+              <td>${course.MAX_CREDIT}</td>
+              <td>${course.ENROLLED}</td>
             </tr>`
   })
 
   document.querySelector('tbody').innerHTML = sortedRows.join('')
-};
+}
