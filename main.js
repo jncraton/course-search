c7fc51789131faae2b30b198ee1344a732ed424e
Peyton <peyton3995@outlook.com>
2024-09-12 22:40:29 -0400

    Adds time search with filter button compatibility

--- main.js
+++ main.js
@@ -26,8 +26,20 @@ document.querySelector('#filter-btn').addEventListener('click', () => {
   let filteredCourses = courses
   // Run your filter here
   // Example: filteredCourses = filterDepartment(filteredCourses)
+  filteredCourses = filterCourseByStartTime(filteredCourses);
 
   console.log('Running filter')
   reloadCourseTable(filteredCourses)
 })
+
+function filterCourseByStartTime(courses){
+  const filterTime = document.getElementById("startTimes").value;
+
+  const filteredCourses = courses.filter(course => {
+    // console.log(course.START_TIME, filterTime, course.START_TIME >= filterTime )
+    return course.START_TIME >= filterTime;
+  })
+
+  return filteredCourses;
+}
 
