63a5ee4b36b8d7f1ac1cd98fab2fb3011d895de6
taigercub <tbvandam@anderson.edu>
2024-09-12 21:42:27 -0400

    Added functionality of online only check box and fixed functionality of pre-requisit check box

--- main.js
+++ main.js
@@ -2,18 +2,38 @@ import { courses } from './courses.js'
 
 
 const rows = courses.map(course => {
-  if (document.getElementById('pre-req' == true)) {
+  if (document.getElementById('pre-req').checked == true) {
     if (course.CONSENT != 'No Special Consent Required') {
       return
     } else {
-      return `<tr>
-            <td>${course.CRSE} - ${course.DESCR}</td>
-          </tr>`
+      if (document.getElementById('online-only').checked == true) {
+        if(course.INSTRUCTION_MODE == 'Face to Face') {
+          return
+        } else {
+          return `<tr>
+                <td>${course.CRSE} - ${course.DESCR} - ${course.CONSENT} - ${course.INSTRUCTION_MODE}</td>
+              </tr>`
+        }
+      } else {
+          return `<tr>
+                <td>${course.CRSE} - ${course.DESCR} - ${course.CONSENT} - ${course.INSTRUCTION_MODE}</td>
+              </tr>`
+      }
     }
   } else {
-    return `<tr>
-          <td>${course.CRSE} - ${course.DESCR}</td>
-        </tr>`
+    if (document.getElementById('online-only').checked == true) {
+        if(course.INSTRUCTION_MODE == 'Face to Face') {
+          return
+        } else {
+          return `<tr>
+                <td>${course.CRSE} - ${course.DESCR} - ${course.CONSENT} - ${course.INSTRUCTION_MODE}</td>
+              </tr>`
+        }
+      } else {
+        return `<tr>
+                  <td>${course.CRSE} - ${course.DESCR} - ${course.CONSENT} - ${course.INSTRUCTION_MODE}</td>
+                </tr>`
+    }
   }
 })
 
