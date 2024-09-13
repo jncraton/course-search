482dff06f92c1911c6d7afe160c6b0ff27be3402
JIckes12 <99823782+JIckes12@users.noreply.github.com>
2024-09-12 21:52:11 -0400

    Changed code in main.js and styles.css to highlight closed courses in red.

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 import { courses } from './courses.js'
 
 const rows = courses.map(course => {
+  let RowEnroll = ''
+  if (course.ENROLLING === 'Closed') {
+    RowEnroll = 'closed-enrollment';
+  }
   if (document.getElementById('pre-req' == true)) {
     if (course.CONSENT != 'No Special Consent Required') {
       return
     } else {
-      return `<tr>
+      return `<tr class="${RowEnroll}">
             <td>${course.CRSE} - ${course.DESCR}</td>
           </tr>`
     }
   } else {
-    return `<tr>
+    return `<tr class="${RowEnroll}">
           <td>${course.CRSE} - ${course.DESCR}</td>
         </tr>`
   }
