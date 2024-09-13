adaab1e676f6860481f041ac078a5b3483f40d73
jepoorbaugh <jepoorbaugh@gmail.com>
2024-09-12 22:32:42 -0400

    Remove unused code

--- main.js
+++ main.js
@@ -21,8 +21,6 @@ document.querySelector('#filter-btn').addEventListener('click', () => {
   reloadCourseTable(filteredCourses)
 })
 
-// --------------------------------
-
 function filterDepartment(courses) {
   // Set our department choice options
   const departmentCode = document.querySelector("#dept-filter").value
@@ -35,25 +33,7 @@ function filterDepartment(courses) {
       return course.CRSE.substring(0, 4) === departmentCode.toUpperCase()
     })
   }
-  return courses
 }
-import { courses } from './courses.js'
-
-// Ethan: filteredCourses: the courses to display 
-let filteredCourses = courses
-
-// Ethan: put code for creating tables in its own function so we can reload tables. 
-function reloadTable() {
-  const rows = filteredCourses.map(course => {
-    return `<tr>
-              <td>${course.CRSE} - ${course.DESCR}</td>
-            </tr>`
-  })
-
-  document.querySelector('tbody').innerHTML = rows.join('')
-}
-
-reloadTable()
 
 // Ethan: load department options and automatically create the selectbox options
 function reloadDepartmentOptions() {
@@ -76,20 +56,3 @@ function reloadDepartmentOptions() {
   document.querySelector('#dept-filter').innerHTML = "<option>NONE</option>" + deptOptionsHTML.join('')
 }
 reloadDepartmentOptions()
-
-// Ethan: Filter courses by department
-function filterDeptOptions() {
-  const departmentCode = document.querySelector("#dept-filter").value
-
-  if (departmentCode === "" || departmentCode === "NONE") {
-    filteredCourses = courses
-  }
-  else {
-    filteredCourses = courses.filter(course => {
-      return course.CRSE.substring(0, 4) === departmentCode.toUpperCase()
-    })
-  }
-  reloadTable()
-}
-
-document.querySelector("#filter-submit").addEventListener("click", filterDeptOptions)
