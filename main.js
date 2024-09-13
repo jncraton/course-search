8d882ac2dfe2e2aa7e9fd82cca2270c5061d8f97
jepoorbaugh <34549460+jepoorbaugh@users.noreply.github.com>
2024-09-12 22:20:44 -0400

    Add button to execute filters (#112)
    * Add boilerplate for a adding filters and running with button click
    
    * Pass linter check
    
    * Fix course table to make it display courses correctly every time
    
    * Fix the issue where `pre-req` checkbox did not get read correctly
    
    * Format code to pass linter
    
    ---------
    
    Co-authored-by: Peyton3995 <143836277+Peyton3995@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
 import { courses } from './courses.js'
 
-const rows = courses.map(course => {
-  if (document.getElementById('pre-req' == true)) {
-    if (course.CONSENT != 'No Special Consent Required') {
-      return
+function reloadCourseTable(filteredCourses) {
+  const rows = filteredCourses.map(course => {
+    if (document.querySelector('#pre-req').checked) {
+      if (course.CONSENT != 'No Special Consent Required') {
+        console.log('Rejected course ', course.CRSE)
+        return
+      } else {
+        return `<tr>
+                <td>${course.CRSE} - ${course.DESCR}</td>
+              </tr>`
+      }
     } else {
       return `<tr>
-            <td>${course.CRSE} - ${course.DESCR}</td>
-          </tr>`
-    }
-  } else {
-    return `<tr>
           <td>${course.CRSE} - ${course.DESCR}</td>
         </tr>`
-  }
-})
+    }
+  })
 
-document.querySelector('tbody').innerHTML = rows.join('')
+  document.querySelector('tbody').innerHTML = rows.join('')
+}
+reloadCourseTable(courses)
+
+document.querySelector('#filter-btn').addEventListener('click', () => {
+  let filteredCourses = courses
+  // Run your filter here
+  // Example: filteredCourses = filterDepartment(filteredCourses)
+
+  console.log('Running filter')
+  reloadCourseTable(filteredCourses)
+})
