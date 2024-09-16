3cc8be22a24358a6fe3d9965149292ec37ef7aff
jepoorbaugh <34549460+jepoorbaugh@users.noreply.github.com>
2024-09-16 18:43:22 -0400

    Filter form (#132)
    * Move filter inputs to form section above table
    
    * Add basic formatting for filter form
    
    * format `index.html` and `main.js` for linter
    
    * Fix formatting of `style.css`
    
    * Remove filter button
    
    ---------
    
    Co-authored-by: dtlemon <124196309+dtlemon@users.noreply.github.com>

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ function reloadCourseTable(filteredCourses) {
   const rows = filteredCourses.map(course => {
     if (document.querySelector('#pre-req').checked) {
       if (course.CONSENT != 'No Special Consent Required') {
-        console.log('Rejected course ', course.CRSE)
         return
       } else {
         return `<tr>
@@ -32,7 +31,7 @@ enrollmentSort.onclick = () => {
   reloadCourseTable(courses)
 }
 
-document.querySelector('#filter-btn').addEventListener('click', () => {
+function applyFilters() {
   let filteredCourses = courses
   // Run your filter here
   // Example: filteredCourses = filterDepartment(filteredCourses)
@@ -40,6 +39,12 @@ document.querySelector('#filter-btn').addEventListener('click', () => {
 
   console.log('Running filter')
   reloadCourseTable(filteredCourses)
+}
+
+// Add event listeners to automatically filter courses
+document.querySelector('#filter-form').addEventListener('change', () => {
+  applyFilters()
+  // return false
 })
 
 function filterDepartment(courses) {
