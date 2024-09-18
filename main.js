c17bb6df1361d9493a1bcde37533d4f2788d8367
taigercub <tbvandam@anderson.edu>
2024-09-18 10:38:24 -0400

    Prettier

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-import { courses } from "./courses.js";
+import { courses } from './courses.js'
 
 function reloadCourseTable(filteredCourses) {
-  const rows = filteredCourses.map((course) => {
-    const rowClass = course.ENROLLING === "Closed" ? "CLOSED" : "";
-    if (document.querySelector("#pre-req").checked) {
-      if (course.CONSENT != "No Special Consent Required") {
-        return;
+  const rows = filteredCourses.map(course => {
+    const rowClass = course.ENROLLING === 'Closed' ? 'CLOSED' : ''
+    if (document.querySelector('#pre-req').checked) {
+      if (course.CONSENT != 'No Special Consent Required') {
+        return
       } else {
         return `<tr>
                 <td>${course.CRSE} - ${course.DESCR}</td>
                 <td>${course.MAX_CREDIT}</td>
                 <td>${course.ENROLLED}</td>
                  <td>${rowClass}</td>
-              </tr>`;
+              </tr>`
       }
     } else {
       return `<tr>
@@ -20,115 +20,115 @@ function reloadCourseTable(filteredCourses) {
           <td>${course.MAX_CREDIT}</td>
           <td>${course.ENROLLED}</td>
           <td>${rowClass}</td>
-        </tr>`;
+        </tr>`
     }
-  });
+  })
 
-  document.querySelector("tbody").innerHTML = rows.join("");
+  document.querySelector('tbody').innerHTML = rows.join('')
 }
-reloadCourseTable(courses);
+reloadCourseTable(courses)
 
-const enrollmentSort = document.getElementById("enrollment");
+const enrollmentSort = document.getElementById('enrollment')
 
-let enrollmentSortCount = 2;
+let enrollmentSortCount = 2
 enrollmentSort.onclick = () => {
   if (enrollmentSortCount % 2 == 0) {
-    courses.sort((b, a) => b.ENROLLED - a.ENROLLED);
-    enrollmentSortCount++;
-    reloadCourseTable(courses);
+    courses.sort((b, a) => b.ENROLLED - a.ENROLLED)
+    enrollmentSortCount++
+    reloadCourseTable(courses)
   } else {
-    courses.sort((a, b) => a.ENROLLED - b.ENROLLED);
-    enrollmentSortCount++;
-    reloadCourseTable(courses);
+    courses.sort((a, b) => a.ENROLLED - b.ENROLLED)
+    enrollmentSortCount++
+    reloadCourseTable(courses)
   }
-};
+}
 
-const credSort = document.getElementById("credits");
-let credSortCount = 2;
+const credSort = document.getElementById('credits')
+let credSortCount = 2
 credSort.onclick = () => {
   if (credSortCount % 2 == 0) {
-    courses.sort((a, b) => b.MAX_CREDIT - a.MAX_CREDIT);
-    reloadCourseTable(courses);
-    credSortCount++;
+    courses.sort((a, b) => b.MAX_CREDIT - a.MAX_CREDIT)
+    reloadCourseTable(courses)
+    credSortCount++
   } else {
-    courses.sort((a, b) => a.MAX_CREDIT - b.MAX_CREDIT);
-    reloadCourseTable(courses);
-    credSortCount++;
+    courses.sort((a, b) => a.MAX_CREDIT - b.MAX_CREDIT)
+    reloadCourseTable(courses)
+    credSortCount++
   }
-};
+}
 
 function applyFilters() {
-  let filteredCourses = courses;
+  let filteredCourses = courses
   // Run your filter here
   // Example: filteredCourses = filterDepartment(filteredCourses)
-  filteredCourses = filterCourseByStartTime(filteredCourses);
-  filteredCourses = filterDepartment(filteredCourses);
-  filteredCourses = OnlineOnly(filteredCourses);
+  filteredCourses = filterCourseByStartTime(filteredCourses)
+  filteredCourses = filterDepartment(filteredCourses)
+  filteredCourses = OnlineOnly(filteredCourses)
 
-  console.log("Running filter");
-  reloadCourseTable(filteredCourses);
+  console.log('Running filter')
+  reloadCourseTable(filteredCourses)
 }
 
 // Add event listeners to automatically filter courses
-document.querySelector("#filter-form").addEventListener("change", () => {
-  applyFilters();
+document.querySelector('#filter-form').addEventListener('change', () => {
+  applyFilters()
   // return false
-});
+})
 
 function OnlineOnly(courses) {
-  if (document.getElementById("online-only").checked == true) {
-    return courses.filter(checkInstruction);
+  if (document.getElementById('online-only').checked == true) {
+    return courses.filter(checkInstruction)
   } else {
-    return courses;
+    return courses
   }
 }
 
 function checkInstruction(course) {
-  return course.INSTRUCTION_MODE != "Face to Face";
+  return course.INSTRUCTION_MODE != 'Face to Face'
 }
 
 function filterDepartment(courses) {
   // Set our department choice options
-  const departmentCode = document.querySelector("#dept-filter").value;
+  const departmentCode = document.querySelector('#dept-filter').value
 
-  if (departmentCode === "" || departmentCode === "NONE") {
-    return courses;
+  if (departmentCode === '' || departmentCode === 'NONE') {
+    return courses
   } else {
-    return courses.filter((course) => {
-      return course.CRSE.substring(0, 4) === departmentCode.toUpperCase();
-    });
+    return courses.filter(course => {
+      return course.CRSE.substring(0, 4) === departmentCode.toUpperCase()
+    })
   }
 }
 
 function filterCourseByStartTime(courses) {
-  const filterTime = document.getElementById("startTimes").value;
+  const filterTime = document.getElementById('startTimes').value
 
-  if (filterTime === "No Specific Time") {
-    return courses;
+  if (filterTime === 'No Specific Time') {
+    return courses
   } else {
-    return courses.filter((course) => {
-      return course.START_TIME === filterTime;
-    });
+    return courses.filter(course => {
+      return course.START_TIME === filterTime
+    })
   }
 }
 
 // Ethan: load department options and automatically create the selectbox options
 function reloadDepartmentOptions() {
-  const deptOptions = [];
+  const deptOptions = []
 
-  const deptOptionsHTML = courses.map((course) => {
-    const dept = course.CRSE.substring(0, 4);
+  const deptOptionsHTML = courses.map(course => {
+    const dept = course.CRSE.substring(0, 4)
 
     if (!deptOptions.includes(dept)) {
-      deptOptions.push(dept);
+      deptOptions.push(dept)
 
-      return `<option>${dept}</option>`;
+      return `<option>${dept}</option>`
     } else {
-      return "";
+      return ''
     }
-  });
+  })
 
-  document.querySelector("#dept-filter").innerHTML =
-    "<option>NONE</option>" + deptOptionsHTML.join("");
+  document.querySelector('#dept-filter').innerHTML =
+    '<option>NONE</option>' + deptOptionsHTML.join('')
 }
-reloadDepartmentOptions();
+reloadDepartmentOptions()
