fb6eb2c1c9dead4a259dd48e497cc9dce411db2a
jillcrawford <jrcrawford@anderson.edu>
2025-09-05 14:28:58 -0400

    added select form in html and pulled into js

--- main.js
+++ main.js
@@ -3,8 +3,14 @@ import { courses } from './courses.js'
 const tbody = document.querySelector('tbody')
 const template = document.querySelector('#courserow')
 
-courses.forEach(course => {
+var selectday = document.getElementById('selecteddays')
+
+courses.filter(course => course.days.includes(selectday.value)).forEach(course => {
   const row = template.content.cloneNode(true)
-  row.querySelector('td').textContent = `${course.crse} - ${course.descr}`
+  row.querySelector('td').textContent = `${course.crse} - ${course.descr} - ${course.days}`
   tbody.append(row)
+})
+
+selectday.addEventListener('change', function() {
+  tbody.innerhtml = ''
 })
