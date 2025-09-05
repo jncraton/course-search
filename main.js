6e4194bd58d9673eba1f20e710defe0b54b6a3d7
Caroline Derolf <cnderolf325@gmail.com>
2025-09-05 14:42:48 -0400

    Implement Online Classes Button

--- main.js
+++ main.js
@@ -8,3 +8,16 @@ courses.forEach(course => {
   row.querySelector('td').textContent = `${course.crse} - ${course.descr}`
   tbody.append(row)
 })
+
+//on click of online selection print online classes only
+button.addEventListener("click",()=> {
+  tbody.innerHTML = ''
+  courses.forEach(course => {
+    if((course.crse[10]=="0")&& course.crse[11]=="E"){
+      const row = template.content.cloneNode(true)
+      row.querySelector('td').textContent = `${course.crse} - ${course.descr}`
+      tbody.append(row)
+    }
+  })
+  });
+ 
