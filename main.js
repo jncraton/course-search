85af2ffe5f23ffacc97707cc25a76653a1f73414
Asher McKenna <mr.ashermckenna@gmail.com>
2025-09-05 14:39:14 -0400

    Split rows for crse and descr

--- main.js
+++ main.js
@@ -5,10 +5,8 @@ const template = document.querySelector('#courserow')
 
 courses.forEach(course => {
   const row = template.content.cloneNode(true)
-  row.querySelector('td').textContent = `${course.crse} - ${course.descr}`
+  row.querySelectorAll('td')[0].textContent = `${course.crse.split("-", 1)}`
+  row.querySelectorAll('td')[1].textContent = `${course.crse.split("-", 2).slice(1)}`
+  row.querySelectorAll('td')[2].textContent = `${course.descr}`
   tbody.append(row)
 })
-
-// get all unique department id's from crse
-// add those to a drop down selector
-// when a dropdown department is selected it refreshes the page to only show those entries
