e793da4db46f85b77809e2f7d7c23d446876f7d1
nickpavey <nicholaspavey03@gmail.com>
2025-09-05 14:11:26 -0400

    Classes with speical consent ordered
    I did the inverse. Hid the classes with No Special Consent and prioritized the classes with special consent (one class)

--- main.js
+++ main.js
@@ -2,9 +2,33 @@ import { courses } from './courses.js'
 
 const tbody = document.querySelector('tbody')
 const template = document.querySelector('#courserow')
+const filterBtn = document.querySelector('#filterConsent')
 
-courses.forEach(course => {
-  const row = template.content.cloneNode(true)
-  row.querySelector('td').textContent = `${course.crse} - ${course.descr}`
-  tbody.append(row)
+// 0 = Show all base case, 1 = consent needed, 2 = no consent needed
+let filterState = 0
+
+function renderTable(filterState) {
+  tbody.innerHTML = '' // clear existing rows. 
+
+  courses.forEach(course => {
+    if (filterState === 1 && course.consent === 'No Consent Required') return
+    if (filterState === 2 && course.consent !== 'No Consent Required') return
+
+    const row = template.content.cloneNode(true)
+    const tds = row.querySelectorAll('td')
+    
+    tds[0].textContent = `${course.crse} - ${course.descr}`
+    tds[1].textContent = course.consent
+
+    tbody.append(row)
+  })
+}
+
+// Initial render
+renderTable(filterState)
+
+// One of the worst button cyclers in the history of man
+filterBtn.addEventListener('click', () => {
+  filterState = (filterState + 1) % 2
+  renderTable(filterState)
 })
