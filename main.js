cf5323e3fe734df885581bd10f66ffc824883e9a
emmanuel garang <emmanuelmayol1844@gmail.com>
2025-09-09 13:58:57 -0400

    sort courses by credits

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ import { courses } from './courses.js'
 const tbody = document.querySelector('tbody')
 const template = document.querySelector('#courserow')
 
+courses.sort((a, b) => a.credits - b.credits)
+
 courses.forEach(course => {
   const row = template.content.cloneNode(true)
-  row.querySelector('td').textContent = `${course.crse} - ${course.descr}`
+  row.querySelector('td').textContent = `${course.crse} - ${course.descr} (${course.credits} cr)`
   tbody.append(row)
 })
