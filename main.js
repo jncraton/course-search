c4f210f06872f217c0ffd2f491f7fb1d3fa56598
Jillian Crawford <jrcrawford@anderson.edu>
2025-09-09 14:06:11 -0400

    Alternating colors for course titles and filter by class day (#160)
    * added stripes to html
    
    * added select form in html and pulled into js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { courses } from './courses.js'
 const tbody = document.querySelector('tbody')
 const template = document.querySelector('#courserow')
 const filterBox = document.querySelector('#filterConsent')
+const selectday = document.getElementById('selecteddays')
 
 function renderTable() {
   tbody.innerHTML = '' // clear rows first
@@ -14,10 +15,22 @@ function renderTable() {
     const row = template.content.cloneNode(true)
     const tds = row.querySelectorAll('td')
 
-    tds[0].textContent = `${course.crse} - ${course.descr}`
+    tds[0].textContent = `${course.crse} - ${course.descr} - ${course.days}`
     tds[1].textContent = course.consent
 
     tbody.append(row)
+  })
+
+  selectday.addEventListener('change', function () {
+    tbody.innerHTML = ''
+    courses
+      .filter(course => course.days.includes(selectday.value))
+      .forEach(course => {
+        const row = template.content.cloneNode(true)
+        row.querySelector('td').textContent =
+          `${course.crse} - ${course.descr} - ${course.days}`
+        tbody.append(row)
+      })
   })
 }
 
