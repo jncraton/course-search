4f90739a1a76a4c55a14974121a339693b71a14f
MacKellar Sample <159157792+msample2@users.noreply.github.com>
2025-09-11 12:56:51 -0400

    Add Liberal Arts filter to course search

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { courses } from './courses.js'
 const tbody = document.querySelector('tbody')
 const template = document.querySelector('#courserow')
 const filterBox = document.querySelector('#filterConsent')
+const liberalArtsCheckbox = document.getElementById("liberalArtsFilter");
 
 function renderTable() {
   tbody.innerHTML = '' // clear rows first
@@ -10,6 +11,12 @@ function renderTable() {
   courses.forEach(course => {
     // If the checkbox is checked, only show "Consent Needed" courses
     if (filterBox.checked && course.consent !== 'Consent Required') return
+
+    // Filter: Liberal Arts in course name/description
+    if (liberalArtsCheckbox.checked &&
+      !(course.descr.includes("Liberal Arts") || (course.title && course.title.includes("Liberal Arts")))) {
+    return
+    }
 
     const row = template.content.cloneNode(true)
     const tds = row.querySelectorAll('td')
@@ -26,3 +33,4 @@ renderTable()
 
 // Re-render whenever checkbox state changes
 filterBox.addEventListener('change', renderTable)
+liberalArtsCheckbox.addEventListener('change', renderTable)
