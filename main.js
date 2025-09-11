d2e122d5ddc855176f4c398174c4628d775258ee
Asher McKenna <mr.ashermckenna@gmail.com>
2025-09-11 10:55:15 -0400

    Add filter section

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ import { courses } from './courses.js'
 
 const tbody = document.querySelector('tbody')
 const template = document.querySelector('#courserow')
+const template = document.querySelector('#filterDepartment')
+
+const getDept = crse => crse.split("-", 1)[0];
+const uniqueDept = [...new Set(getDept)];
+console.log(uniqueDept);
+
+function renderTable() {
+
+  tbody.innerHTML = ''
 
 courses.forEach(course => {
   const row = template.content.cloneNode(true)
@@ -10,3 +19,6 @@ courses.forEach(course => {
   row.querySelectorAll('td')[2].textContent = `${course.descr}`
   tbody.append(row)
 })
+}
+renderTable()
+filterBox.addEventListener('change', renderTable)
