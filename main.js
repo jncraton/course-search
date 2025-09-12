592637d6ae5ae72deb9dac3e1708a6b314804f0f
Caroline Derolf <cnderolf325@gmail.com>
2025-09-12 14:32:00 -0400

    remove c.crse- unnecessary

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function filterCourses() {
   //If the checkbox is checked, show online courses
   if (filterOnline.checked) {
     arr = arr.filter(
-      c => c.crse && c['instruction mode'] === 'Asynchronous Online',
+      c => c['instruction mode'] === 'Asynchronous Online',
     )
   }
 
